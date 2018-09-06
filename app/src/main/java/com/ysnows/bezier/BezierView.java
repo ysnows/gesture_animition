4d4207b20444279ed27ad2de083a002f62a47ccd
咸光金 <ysnows@163.com>
2018-09-06 14:39:30 +0800

    1. first cm

--- app/src/main/java/com/ysnows/bezier/BezierView.java
+++ app/src/main/java/com/ysnows/bezier/BezierView.java
@@ -1,9 +1,11 @@
 package com.ysnows.bezier;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
@@ -13,6 +15,7 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,12 +28,23 @@ import java.util.List;
 
 public class BezierView extends View {
 
+    public String position = "right";
 
     private Path path;
     private Paint paint;
     private int slice;
-    public int height;
-
+    public PointF center;
+    public int maxHeight = 80;
+    public int bgColor = Color.parseColor("#C3000000");
+    private PointF pOne;
+    private PointF pControlOne;
+    private PointF pControlTwo;
+    private PointF pControlThree;
+    private PointF pControlFour;
+    private PointF PTwo;
+    private PointF PThree;
+    private int screenWidthPixels;
+    private float v;
 
     public BezierView(Context context) {
         this(context, null);
@@ -39,7 +53,6 @@ public class BezierView extends View {
 
     public BezierView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
-
 
     }
 
@@ -50,32 +63,189 @@ public class BezierView extends View {
     }
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        int screenWidthPixels = UiUtils.getScreenWidthPixels();
-
+        screenWidthPixels = UiUtils.getScreenWidthPixels();
+//      maxHeight = UiUtils.dp2Px(getContext(), 35);
         path = new Path();
-
         paint = new Paint();
-        paint.setColor(Color.parseColor("#9B9B9B"));
+        paint.setColor(bgColor);
         paint.setAntiAlias(true);//设置抗锯齿
         paint.setStyle(Paint.Style.FILL);
         paint.setStrokeWidth(10);
         slice = screenWidthPixels / 10;
 
-        height = 0;
-    }
+        center = new PointF(0, 0);
 
+        pOne = new PointF();
+        pControlOne = new PointF();
+        pControlTwo = new PointF();
+        pControlThree = new PointF();
+        pControlFour = new PointF();
+        PTwo = new PointF();
+        PThree = new PointF();
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         path.reset();
-        path.moveTo(slice * 1.8f, 0);
-        float firstAnchor = 4f;
-        float sencondaryAnchor = 4.2f;
-        path.cubicTo(slice * firstAnchor, height * 0.4f, slice * sencondaryAnchor, height, slice * 5, height);
 
-        path.cubicTo(slice * 5.8f, height, slice * (10 - firstAnchor), height * 0.4f, slice * 8.2f, 0);
+        if ("top".equals(position)) {
+            pOne.set(center.x - 3.2f * slice, 0);
+            pControlOne.set(center.x - 1.4f * slice, 0.5f * center.y);
+            pControlTwo.set(center.x - 0.8f * slice, center.y);
+            PTwo.set(center.x, center.y);
+            pControlThree.set(center.x + 0.8f * slice, center.y);
+            pControlFour.set(center.x + 1.4f * slice, 0.5f * center.y);
+            PThree.set(center.x + 3.2f * slice, 0);
+        } else if ("bottom".equals(position)) {
+            pOne.set(center.x - 3.2f * slice, getMeasuredHeight());
+            pControlOne.set(center.x - 1.4f * slice, getMeasuredHeight() - 0.5f * center.y);
+            pControlTwo.set(center.x - 0.8f * slice, getMeasuredHeight() - center.y);
+            PTwo.set(center.x, getMeasuredHeight() - center.y);
+            pControlThree.set(center.x + 0.8f * slice, getMeasuredHeight() - center.y);
+            pControlFour.set(center.x + 1.4f * slice, getMeasuredHeight() - 0.5f * center.y);
+            PThree.set(center.x + 3.2f * slice, getMeasuredHeight());
+        } else if ("left".equals(position)) {
+            pOne.set(0, center.y - 3.2f * slice);
+            pControlOne.set(0.5f * center.x, center.y - 1.4f * slice);
+            pControlTwo.set(center.x, center.y - 0.8f * slice);
+            PTwo.set(center.x, center.y);
+            pControlThree.set(center.x, center.y + 0.8f * slice);
+            pControlFour.set(0.5f * center.x, center.y + 1.4f * slice);
+            PThree.set(0, center.y + 3.2f * slice);
+        } else if ("right".equals(position)) {
+            pOne.set(screenWidthPixels, center.y - 3.2f * slice);
+            pControlOne.set(screenWidthPixels - 0.5f * center.x, center.y - 1.4f * slice);
+            pControlTwo.set(screenWidthPixels - center.x, center.y - 0.8f * slice);
+            PTwo.set(screenWidthPixels - center.x, center.y);
+            pControlThree.set(screenWidthPixels - center.x, center.y + 0.8f * slice);
+            pControlFour.set(screenWidthPixels - 0.5f * center.x, center.y + 1.4f * slice);
+            PThree.set(screenWidthPixels, center.y + 3.2f * slice);
+        }
+
+        path.moveTo(pOne.x, pOne.y);
+        path.cubicTo(pControlOne.x, pControlOne.y, pControlTwo.x, pControlTwo.y, PTwo.x, PTwo.y);
+        path.cubicTo(pControlThree.x, pControlThree.y, pControlFour.x, pControlFour.y, PThree.x, PThree.y);
         path.close();
         canvas.drawPath(path, paint);
+
+//        path.moveTo(slice * 1.8f, 0);
+//        float firstAnchor = 3.6f;
+//        float sencondaryAnchor = 4.2f;
+//        path.cubicTo(slice * firstAnchor, center.y * 0.5f, slice * sencondaryAnchor, center.y, slice * 5, center.y);
+//        path.cubicTo(slice * 5.8f, center.y, slice * (10 - firstAnchor), center.y * 0.5f, slice * 8.2f, 0);
+//        path.close();
+//        canvas.drawPath(path, paint);
+    }
+
+    /**
+     * 处理滑动手势，在 onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) 的时候调用
+     *
+     * @param distanceX
+     * @param distanceY
+     */
+    public void handleScroll(float distanceX, float distanceY) {
+
+        if (position.equals("top") || position.equals("bottom")) {//如果是在顶部或者底部，处理下滑||上滑的动作
+            if (distanceY < 0) {//下滑
+                if (position.equals("bottom")) {
+                    this.v -= Math.abs(distanceY);
+                } else if (position.equals("top")) {
+                    this.v += Math.abs(distanceY);
+                }
+
+                if (this.v > 255) {
+                    center.y = maxHeight;
+                } else {
+                    center.y = (int) ((this.v / 255) * maxHeight);
+                }
+
+            } else if (distanceY > 0) {
+                if (position.equals("bottom")) {
+                    this.v += Math.abs(distanceY);
+                } else if (position.equals("top")) {
+                    this.v -= Math.abs(distanceY);
+                }
+
+                if (this.v > 255) {
+                    center.y = maxHeight;
+                } else {
+                    center.y = (int) ((this.v / 255) * maxHeight);
+                }
+
+            }
+        }
+
+
+        if (position.equals("left") || position.equals("right")) {//如果是在左侧或者右侧，处理左滑||右滑的动作
+            if (distanceX < 0) {//右滑
+                if (position.equals("left")) {
+                    this.v += Math.abs(distanceX);
+                } else if (position.equals("right")) {
+                    this.v -= Math.abs(distanceX);
+                }
+
+                if (this.v > 255) {
+                    center.x = maxHeight;
+                } else {
+                    center.x = (int) ((this.v / 255) * maxHeight);
+                }
+
+            } else if (distanceX > 0) {//左滑
+                if (position.equals("right")) {
+                    this.v += Math.abs(distanceX);
+                } else if (position.equals("left")) {
+                    this.v -= Math.abs(distanceX);
+                }
+
+                if (this.v > 255) {
+                    center.x = maxHeight;
+                } else {
+                    center.x = (int) ((this.v / 255) * maxHeight);
+                }
+            }
+        }
+        invalidate();
+    }
+
+
+    /**
+     * 设置手势的中点，在OnDown(MotionEvent e)的时候调用
+     *
+     * @param e
+     */
+    public void initCenter(MotionEvent e) {
+        if (position.equals("top") || position.equals("bottom")) {
+            center.x = e.getX();
+        } else if (position.equals("left") || position.equals("right")) {
+            center.y = e.getY();
+        }
+    }
+
+    /**
+     * 处理松手后的操作
+     */
+    public void handleActionUp() {
+        final float nowHeight = center.y;
+        final float nowWidth = center.x;
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(1F, 0F);
+        valueAnimator.setDuration(300);
+        valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Object animatedValue = animation.getAnimatedValue();
+                Float value = (Float) animatedValue;
+                if (position.equals("top") || position.equals("bottom")) {
+                    center.y = (int) (nowHeight * value);
+                } else if (position.equals("left") || position.equals("right")) {
+                    center.x = (int) (nowWidth * value);
+                }
+                postInvalidate();
+                v = 0;
+            }
+        });
+        valueAnimator.start();
+
     }
 }
--- app/src/main/java/com/ysnows/bezier/MainActivity.java
+++ app/src/main/java/com/ysnows/bezier/MainActivity.java
@@ -1,29 +1,15 @@
 package com.ysnows.bezier;
 
-import android.animation.ValueAnimator;
-import android.graphics.PointF;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.LinearInterpolator;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
-    private int screenWidthPixels;
-    private int slice;
-    private View root;
     private GestureDetector gestureDetector;
-    private float downX;
-    private float downY;
     private BezierView bezier;
-    private float v;
+
 
     /**
      * @param savedInstanceState
@@ -34,7 +20,6 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         setContentView(R.layout.activity_main);
 
         gestureDetector = new GestureDetector(this, this);
-        root = findViewById(R.id.root);
         bezier = findViewById(R.id.bezier);
     }
 
@@ -43,65 +28,7 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         int action = event.getAction();
 
         if (action == MotionEvent.ACTION_UP) {
-            final int nowHeight = bezier.height;
-            ValueAnimator valueAnimator = ValueAnimator.ofFloat(1F, 0F);
-            valueAnimator.setDuration(300);
-            valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    Object animatedValue = animation.getAnimatedValue();
-                    Float value = (Float) animatedValue;
-                    bezier.height = (int) (nowHeight * value);
-                    if (BuildConfig.DEBUG)
-                        Log.d(this.getClass().getName(), "bezier.height:" + bezier.height);
-                    bezier.postInvalidate();
-                    v = 0;
-                }
-            });
-            valueAnimator.start();
-
-
-            if (BuildConfig.DEBUG) Log.d(this.getClass().getName(), "up:" + action);
-
-            float x = event.getX();
-            float y = event.getY();
-
-            float distanceX = x - downX;
-            float distanceY = y - downY;
-
-            Log.d(this.getClass().getName(), "onUp:" + distanceX + "  Y:" + distanceY + "  time:" + System.currentTimeMillis());
-
-
-            float absX = Math.abs(distanceX);
-            float absY = Math.abs(distanceY);
-
-
-            if (Math.max(absX, absY) < 40) {
-                return false;
-            }
-
-            String userAction = null;
-
-            if (absX > absY) {
-                //左右滑动
-                if (distanceX < 0) {
-                    //往左滑动
-                } else {
-                    //往右滑动
-                }
-
-            } else {
-                //上下滑动
-
-                if (distanceY < 0) {
-                    //往上滑动
-                } else {
-                    //往下滑动
-
-                }
-            }
-
+            bezier.handleActionUp();
 
             return super.onTouchEvent(event);
 
@@ -112,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     @Override
     public boolean onDown(MotionEvent e) {
+        bezier.initCenter(e);
+
         return false;
     }
 
@@ -127,28 +56,7 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        if (BuildConfig.DEBUG) Log.d(this.getClass().getName(), distanceY + "Y");
-        if (distanceY < 0) {//下滑
-            v += Math.abs(distanceY);
-            if (v > 255) {
-                bezier.height = 100;
-            } else {
-                bezier.height = (int) ((v / 255) * 100);
-            }
-
-            bezier.invalidate();
-
-        } else if (distanceY > 0) {
-            v -= Math.abs(distanceY);
-            if (v > 255) {
-                bezier.height = 100;
-            } else {
-                bezier.height = (int) ((v / 255) * 100);
-            }
-
-            bezier.invalidate();
-        }
-
+        bezier.handleScroll(distanceX, distanceY);
         return false;
     }
 
